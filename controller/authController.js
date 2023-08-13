6a034b12ddfd512c30044e77a5aefb4382fa4137
akhandpratap14 <akhandpratap892@gmail.com>
2023-08-13 11:00:52 +0530

    login functionalities changes

--- controller/authController.js
+++ controller/authController.js
@@ -70,6 +70,33 @@ exports.login = catchAsync( async (req, res, next) => {
     createSendToken(user, 201, res);
 });
 
+exports.isLoggedIn = catchAsync(async(req, res, next) => {
+
+    if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+
+    // 2. Verification token 
+    const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+
+    // 3. Check if user still exists
+    const freshUser = await User.findById(decoded.id);
+    if(!freshUser){
+        return next();
+    }
+
+    // 4. Check if user changed password after the token was issued
+    if(freshUser.changedPasswordAfter(decoded.iat)){
+        return next();
+    }
+
+    // THERE IS ALOGGED IN USER
+    res.locals.user = freshUser
+    next();
+    }
+    next();
+});
+
+
 exports.protect = catchAsync(async(req, res, next) => {
 
     // 1. Getting the token and check if its exist
